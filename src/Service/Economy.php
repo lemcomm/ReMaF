fbe151378bb886d9bfa51f9560dfdd5718a54c68
Andrew <Zanaras@users.noreply.github.com>
2023-01-16 01:26:30 -0600

    Update Economy.php

--- src/Service/Economy.php
+++ src/Service/Economy.php
@@ -592,7 +592,7 @@ class Economy {
 				foreach ($unit->getSupplies() as $supply) {
 					if ($supply->getType() === 'food') {
 						$found = true;
-						$supply->setQuantity($supply->getQuantity()+$supply->getQuantity());
+						$supply->setQuantity($supply->getQuantity()+ceil($qty));
 						break;
 					}
 				}
