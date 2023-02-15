6b0838435dc2adc586f1685319ca2241729c2d3e
Andrew <Zanaras@users.noreply.github.com>
2023-02-14 20:26:18 -0600

    Update EventLog.php

--- src/Entity/EventLog.php
+++ src/Entity/EventLog.php
@@ -9,40 +9,43 @@ class EventLog {
 
 
 	public function getType() {
-      		if ($this->settlement) return 'settlement';
-      		if ($this->realm) return 'realm';
-      		if ($this->character) return 'character';
-      		if ($this->place) return 'place';
-      		if ($this->house) return 'house';
-      		if ($this->quest) return 'quest';
-      		if ($this->artifact) return 'artifact';
-      		if ($this->association) return 'association';
-      		return false;
-      	}
+		if ($this->settlement) return 'settlement';
+		if ($this->realm) return 'realm';
+		if ($this->character) return 'character';
+		if ($this->unit) return 'unit';
+		if ($this->place) return 'place';
+		if ($this->house) return 'house';
+		if ($this->quest) return 'quest';
+		if ($this->artifact) return 'artifact';
+		if ($this->association) return 'association';
+		return false;
+	}
 
 	public function getSubject() {
-      		if ($this->settlement) return $this->settlement;
-      		if ($this->realm) return $this->realm;
-      		if ($this->character) return $this->character;
-      		if ($this->place) return $this->place;
-      		if ($this->house) return $this->house;
-      		if ($this->quest) return $this->quest;
-      		if ($this->artifact) return $this->artifact;
-      		if ($this->association) return $this->association;
-      		return false;
-      	}
+		if ($this->settlement) return $this->settlement;
+		if ($this->realm) return $this->realm;
+		if ($this->character) return $this->character;
+		if ($this->unit) return $this->unit;
+		if ($this->place) return $this->place;
+		if ($this->house) return $this->house;
+		if ($this->quest) return $this->quest;
+		if ($this->artifact) return $this->artifact;
+		if ($this->association) return $this->association;
+		return false;
+	}
 
 	public function getName() {
-      		if ($this->settlement) return $this->settlement->getName();
-      		if ($this->realm) return $this->realm->getName();
-      		if ($this->character) return $this->character->getName();
-      		if ($this->place) return $this->place->getName();
-      		if ($this->house) return $this->house->getName();
-      		if ($this->quest) return $this->quest->getSummary();
-      		if ($this->artifact) return $this->artifact->getName();
-      		if ($this->association) return $this->association->getName();
-      		return false;
-      	}
+		if ($this->settlement) return $this->settlement->getName();
+		if ($this->realm) return $this->realm->getName();
+		if ($this->character) return $this->character->getName();
+		if ($this->unit) return $this->unit->getSettings()->getName();
+		if ($this->place) return $this->place->getName();
+		if ($this->house) return $this->house->getName();
+		if ($this->quest) return $this->quest->getSummary();
+		if ($this->artifact) return $this->artifact->getName();
+		if ($this->association) return $this->association->getName();
+		return false;
+	}
 
     /**
      * @var integer
