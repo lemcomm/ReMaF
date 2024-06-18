f114c22b1d451301a95fe38520ac5941066d0fe6
Andrew Gwynn <Zanaras@users.noreply.github.com>
2024-06-17 21:16:00 -0500

    Update turn commands

--- hourly.sh
+++ hourly.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-LOGDIR="/var/maf/logs"
-APP="/var/www/maf/app/console"
+LOGDIR="/var/www/maf/var/log"
+APP="/var/www/maf/bin/console"
+DAY=`date +%a%H`
 
-#LOGDIR="/tmp"
-#APP="./app/console"
-
-# Fix the fucking permissions that never stick:
+# Fix the permissions that never stick:
 # sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 # sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 
-php $APP --env=prod maf:process:activities 2>&1 >> $LOGDIR/hourly.log
-php $APP --env=prod maf:process:familiarity -t 2>&1 >> $LOGDIR/hourly.log
-php $APP --env=prod maf:process:travel -t 2>&1 >> $LOGDIR/hourly.log
-php $APP --env=prod maf:process:spotting -t 2>&1 >> $LOGDIR/hourly.log
-php $APP --env=prod maf:run -t -d hourly 2>&1 >> $LOGDIR/hourly.log
-php $APP --env=prod dungeons:hourly -d 2>&1 >> $LOGDIR/hourly.log
+php $APP --env=prod maf:process:activities 2>&1 > $LOGDIR/hourly-$DAY.log
+php $APP --env=prod maf:process:familiarity -t 2>&1 >> $LOGDIR/hourly-$DAY.log
+php $APP --env=prod maf:process:travel -t 2>&1 >> $LOGDIR/hourly-$DAY.log
+php $APP --env=prod maf:process:spotting -t 2>&1 >> $LOGDIR/hourly-$DAY.log
+php $APP --env=prod maf:run -t -d hourly 2>&1 >> $LOGDIR/hourly-$DAY.log
+php $APP --env=prod dungeons:hourly -d 2>&1 >> $LOGDIR/hourly-$DAY.log
 echo "----- hourly done -----" >> $LOGDIR/hourly.log
--- quarterhourly.sh
+++ quarterhourly.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-LOGDIR="/var/www/maf/app/logs"
-APP="/var/www/maf/app/console"
+LOGDIR="/var/www/maf/var/log"
+APP="/var/www/maf/bin/console"
+DAY=`date +%a%H`
 
-#LOGDIR="/tmp"
-#APP="./app/console"
-
-# Fix the fucking permissions that never stick:
+# Fix the permissions that never stick:
 # sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 # sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 
-
-php $APP --env=prod maf:process:battles -t 5 2>&1 >> $LOGDIR/quarterhourly.log
-php $APP --env=prod maf:mail 2>&1 >> $LOGDIR/quarterhourly.log
+php $APP --env=prod maf:process:battles -t 5 2>&1 > $LOGDIR/quarterhourly-$DAY.log
+php $APP --env=prod maf:mail 2>&1 >> $LOGDIR/quarterhourly-$DAY.log
