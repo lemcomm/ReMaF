162d43353a023ea339d35eee80599bd0691a8431
Andrew Gwynn <Zanaras@users.noreply.github.com>
2024-12-31 20:04:39 -0600

    Cleanup log generation and removal

--- quarterhourly.sh
+++ quarterhourly.sh
@@ -2,11 +2,11 @@
 
 LOGDIR="/var/www/maf/var/log"
 APP="/var/www/maf/bin/console"
-DAY=`date +%a%H`
+DAY=`date +%a`
 
 # Fix the permissions that never stick:
 # sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 # sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 
-php $APP maf:process:battles -t 5 2>&1 > $LOGDIR/quarterhourly-$DAY.log
+php $APP maf:process:battles -t 5 2>&1 >> $LOGDIR/quarterhourly-$DAY.log
 php $APP maf:mail 2>&1 >> $LOGDIR/quarterhourly-$DAY.log
